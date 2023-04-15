b996f8a0ebf2812bfc1e8e8f2b0f750181a65971
Cromuel Oliver <cromuelbarut@gmail.com>
2023-04-15 12:24:06 +0800

    created simple home page

---
+++ lib/pages/home_page/components/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'home_app_bar.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: HomeAppBar(),
+      ),
+      body: Center(
+        child: Text('HOME PAGE'),
+      ),
+    );
+  }
+}
